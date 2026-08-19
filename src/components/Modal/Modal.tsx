3f8dc582ab822377608ffa481ea7abd246886df1
React Practice <dev@example.com>
2026-08-18 18:05:55 -0700

    Phase-4: UseRef understood, skipping for now.

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,4 +1,4 @@
-import type { JSX } from "react";
+import { useRef, type JSX } from "react";
 import { createPortal } from "react-dom";
 import "./Modal.css";
 
@@ -8,6 +8,12 @@ interface ModalProps {
 }
 
 const Modal = ({ element, onClose }: ModalProps) => {
+
+  //TBD - use cases understood for each, skipping useRef this phase for time.
+  const closeButtonRef = useRef<HTMLButtonElement>(null);
+  // const previouslyFocusedRef = useRef<HTMLElement>(null);
+  const modalRef = useRef<HTMLDivElement>(null);
+
   const handleBackdropClick = (event: React.MouseEvent<HTMLDivElement>) => {
     if (event.target === event.currentTarget) {
       onClose();
@@ -16,8 +22,8 @@ const Modal = ({ element, onClose }: ModalProps) => {
 
   return createPortal(
     <div className="modal" onClick={handleBackdropClick}>
-      <div className="modal-content">
-        <button className="modal-close" onClick={onClose} aria-label="Close">
+      <div className="modal-content" ref={modalRef}>
+        <button ref={closeButtonRef} className="modal-close" onClick={onClose} aria-label="Close">
           X
         </button>
         {element}
